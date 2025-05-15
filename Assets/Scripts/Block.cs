0722fdf4a58d4e85fb95dfa3a2804429cc1c379f
jglee <jglee@naver.com>
2025-05-15 12:24:19 +0900

    - 점수 시스템과 시간 제한 및 결과 UI 추가 - 줄 제거 특수 블록 및 순차 제거 연출 구현 - 스왑 및 연쇄 처리 중 입력 방지 처리 - 선택된 블록에 스케일 애니메이션 적용

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,11 +1,17 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 public enum BlockType
 {
+    // 일반 블록
     Apple,
     Banana,
     Orange,
     Grape,
     // 필요시 추가
+
+    // 특수 블록
+    RowClear, // 가로줄 제거 특수 블록
+    ColClear,  // 세로줄 제거 특수 블록
 }
 public class Block : MonoBehaviour
 {
@@ -15,7 +21,12 @@ public class Block : MonoBehaviour
     public BlockType blockType;
     public SpriteRenderer spriteRenderer;
 
+    public bool isSpecial = false;  // 특수 블록 체크
+    public bool isRowClear = false; // true : 가로줄 , false : 세로줄
+
     private GridManager gridManager;
+
+
 
     void Start()
     {
@@ -26,6 +37,15 @@ public class Block : MonoBehaviour
     {
         blockType = type;
         spriteRenderer.sprite = sprite;
+
+        if (isSpecial)
+        {
+            spriteRenderer.color = isRowClear ? Color.red : Color.cyan;
+        }
+        else
+        {
+            spriteRenderer.color = Color.white;
+        }
     }
 
     void OnMouseDown()
