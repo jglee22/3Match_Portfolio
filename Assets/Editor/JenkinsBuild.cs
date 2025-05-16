4bf46f89c7b5742a4b588419045c543086eb93bd
jglee <jglee@naver.com>
2025-05-16 13:26:50 +0900

    주석 수정 및 추가 젠킨스 빌드용 스크립트 추가

---
+++ Assets/Editor/JenkinsBuild.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class JenkinsBuild : MonoBehaviour
+{
+    static string[] SCENES = FindEnabledEditorScenes();
+
+    static void PerformBuild()
+    {
+        BuildPipeline.BuildPlayer(FindEnabledEditorScenes(), "Builds/Windows/MyGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+    }
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+        return EditorScenes.ToArray();
+    }
+}
